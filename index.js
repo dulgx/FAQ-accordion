3c17cda014c071516c0110fc7e39e3c73999250d
dulgx <dulguunnu@gmail.com>
2024-01-18 16:59:14 +0800

    done

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const questions = document.querySelectorAll(".quess");
 
 questions.forEach(function (question) {
     const img = question.querySelector(".img");
-    const answer = question.nextElementSibling; // Assuming the answer follows the question in the HTML structure
+    const answer = question.nextElementSibling;
 
     question.addEventListener("click", function () {
-        // Toggle the active class on the question
         question.classList.toggle("active");
 
-        // Toggle the show class on the corresponding answer
         if (question.classList.contains("active")) {
             img.src = "./assets/images/icon-minus.svg";
             answer.classList.add('show');
